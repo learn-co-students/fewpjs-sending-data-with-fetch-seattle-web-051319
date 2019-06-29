4b35d5f6bc6bfa64dea1c7676b1cb205e20ffce5
fut0k0 <cccabell@icloud.com>
2019-06-28 17:54:15 -0700

    Passing lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(userName, userEmail) {
+	let submitUrl = "http://localhost:3000/users";
+	let formData = {
+		name: userName,
+		email: userEmail
+	};
+	
+	let configObj = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+		body: JSON.stringify(formData)
+	};
+	
+	return fetch(submitUrl, configObj)
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(json) {
+			document.body.textContent = json.id;
+		})
+		.catch(function(error) {
+			document.body.textContent = error.message;
+		});
+}
