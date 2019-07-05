7d44e0ed561ec418a7b4ce66592e261412ac3a91
chetchopra <chetanya.chopra94@gmail.com>
2019-07-05 10:10:45 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+function submitData(name, email) {
+  let arr = {name: name, email: email};
+  let configObj = 
+    { headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: "POST",
+    body: JSON.stringify(arr)};
+
+
+  fetch("http://localhost:3000/users", configObj)
+  .then(function (response) {
+    return response.json();
+  }).then(function (json) {
+    addToDom(json);
+  }).catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  });
+}
+
+function addToDom (data) {
+  let h = document.createElement("h2");
+  h.textContent = data.id;
+  let body = document.getElementById("shit")
+  body.appendChild(h);
+}
+
+submitData("Tom", "email");
